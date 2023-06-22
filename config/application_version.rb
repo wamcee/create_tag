0df6ebad3edb1be94731d35c5ee955bf94ae41ec
wamcee <vamshidhar.reddy@meisterlabs.com>
2023-06-22 16:29:25 +0200

    add application version file

---
+++ config/application_version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# The application version. This should be loaded before other initializers that depend on it (e.g. Bugsnag, OTEL).
+class MeisterTask
+    VERSION = 'v23.06.22.101831'
+  end
+  
